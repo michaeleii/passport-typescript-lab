6a29d76d4ec853da5fe02b045f3744d5e012024a
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-02-06 16:01:36 -0800

    Fix userController types

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,27 +1,32 @@
-import {userModel} from "../models/userModel";
+import { userModel } from "../models/userModel";
 
 const getUserByEmailIdAndPassword = (email: string, password: string) => {
-  let user = userModel.findOne(email);
-  if (user) {
-    if (isUserValid(user, password)) {
-      return user;
-    }
-  }
-  return null;
+	let user = userModel.findOne(email);
+	if (user) {
+		if (isUserValid(user, password)) {
+			return user;
+		}
+	}
+	return null;
 };
-const getUserById = (id:any) => {
-  let user = userModel.findById(id);
-  if (user) {
-    return user;
-  }
-  return null;
+const getUserById = (id: number) => {
+	let user = userModel.findById(id);
+	if (user) {
+		return user;
+	}
+	return null;
 };
 
-function isUserValid(user: any, password: string) {
-  return user.password === password;
+function isUserValid(
+	user: {
+		id: number;
+		name: string;
+		email: string;
+		password: string;
+	},
+	password: string
+) {
+	return user.password === password;
 }
 
-export {
-  getUserByEmailIdAndPassword,
-  getUserById,
-};
+export { getUserByEmailIdAndPassword, getUserById };
