eaa83c762225bfb00f297de425b558be9ba102ed
Michael Lei <michaelleie0ca@gmail.com>
2023-02-11 21:00:30 -0800

    Fix types for github strategy

--- middleware/passportStrategies/githubStrategy.ts
+++ middleware/passportStrategies/githubStrategy.ts
@@ -1,21 +1,28 @@
-import { Strategy as GitHubStrategy } from 'passport-github2';
-import { PassportStrategy } from '../../interfaces/index';
+import { Request } from "express";
+import { Strategy as GitHubStrategy } from "passport-github2";
+import { PassportStrategy } from "../../interfaces/index";
 
 const githubStrategy: GitHubStrategy = new GitHubStrategy(
-    {
-        clientID: "",
-        clientSecret: "",
-        callbackURL: "",
-        passReqToCallback: true,
-    },
-    
-    /* FIX ME 😭 */
-    async (req: any, accessToken: any, refreshToken: any, profile: any, done: any) => {},
+	{
+		clientID: "",
+		clientSecret: "",
+		callbackURL: "",
+		passReqToCallback: true,
+	},
+
+	/* Done */
+	async (
+		req: Request,
+		accessToken: string,
+		refreshToken: string,
+		profile: any,
+		done: (err?: Error | null, profile?: any) => void
+	) => {}
 );
 
 const passportGitHubStrategy: PassportStrategy = {
-    name: 'github',
-    strategy: githubStrategy,
+	name: "github",
+	strategy: githubStrategy,
 };
 
 export default passportGitHubStrategy;
