ecf75dbc55bc76beab11f32ec23dbd5147b0fcf5
Michael Lei <michaelleie0ca@gmail.com>
2023-02-12 11:31:51 -0800

    Setup connect-flash

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
 import path from "path";
 import passportMiddleware from "./middleware/passportMiddleware";
+import flash from "connect-flash";
 
 const port = process.env.port || 8000;
 
--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -13,6 +13,7 @@ router.post(
 	passport.authenticate("local", {
 		successRedirect: "/dashboard",
 		failureRedirect: "/auth/login",
+		failureFlash: "Invalid username or password.",
 	})
 );
 
